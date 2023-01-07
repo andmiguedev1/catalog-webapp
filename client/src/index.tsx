1d2193a279059f075323dcc79c4ec2c40cce748b
Miguel Gomez <andresmgomez23@gmail.com>
2023-01-07 15:44:06 -0500

    Setup a folder structure for react client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import App from './app/layout/App';
+
+import './app/layout/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
