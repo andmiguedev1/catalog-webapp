a70e5e2bd5344f4731951628f8136d890e582b4e
Miguel Gomez <andresmgomez23@gmail.com>
2023-01-12 18:57:41 -0500

    Add migration to create a cart items table

---
+++ api/Entities/Cart.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace api.Entities
+{
+   public class Cart
+   {
+      public int Id { get; set; }
+      public string CustomerId { get; set; }
+      public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+      public void AddCartItem(Product product, int quantity)
+      {
+         if (CartItems.All(customerItem => customerItem.ProductId != product.Id))
+         {
+            CartItems.Add(new CartItem
+            {
+               Product = product,
+               Quantity = quantity
+            });
+         }
+
+         var currentCart = CartItems.FirstOrDefault(cartItem => cartItem.ProductId == product.Id);
+
+         if (currentCart != null)
+         {
+            currentCart.Quantity += quantity;
+         }
+      }
+
+      public void RemoveCartItem(int productId, int quantity)
+      {
+        // 
+        var currentCart = CartItems.FirstOrDefault(cartItem => cartItem.ProductId == productId);
+        
+        //
+        if (currentCart == null) return;
+
+        //
+        currentCart.Quantity -= quantity;
+      }
+   }
+}
---
+++ api/Entities/CartItem.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Entities
+{
+   [Table("CartItems")]
+   public class CartItem
+   {
+      public int Id { get; set; }
+      public int Quantity { get; set; }
+      public int ProductId { get; set; }
+      public Product Product { get; set; }
+      public int CartId { get; set; }
+      public Cart Carts { get; set; }
+   }
+}
