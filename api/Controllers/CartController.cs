c8ab4a984dc9080b3c3ba721285fde48258020e3
Miguel Gomez <andresmgomez23@gmail.com>
2023-01-13 21:27:02 -0500

    Refactor endpoints to update resources

--- api/Controllers/CartController.cs
+++ api/Controllers/CartController.cs
@@ -19,7 +19,7 @@ namespace api.Controllers
 
       }
 
-      [HttpGet]
+      [HttpGet(Name = "GetShoppingCart")]
       public async Task<ActionResult<CartDto>> GetShoppingCart()
       {
          var shoppingCart = await GetShoppingItems();
@@ -31,29 +31,15 @@ namespace api.Controllers
          };
 
          // Set the structure return by the API
-         // using Data Transfer Objects
-         return new CartDto
-         {
-            Id = shoppingCart.Id,
-            CustomerId = shoppingCart.CustomerId,
-            CartItems = shoppingCart.CartItems.Select(cartItem => new CartItemDto
-            {
-               ProductId = cartItem.ProductId,
-               Name = cartItem.Product.Name,
-               Image = cartItem.Product.Image,
-               Brand = cartItem.Product.Brand,
-               Type = cartItem.Product.Type,
-               Price = cartItem.Product.Price,
-               Quantity = cartItem.Quantity
-            }).ToList()
-         };
+         // using Data Transfer Objects 
+         return MapCartItemsToDto(shoppingCart);
       }
 
       [HttpPost]
       // api/cart?productId=5&quantity=4
-      public async Task<ActionResult> AddShoppingItemsToCart(int productId, int quantity)
+      public async Task<ActionResult<CartDto>> AddShoppingItemsToCart(int productId, int quantity)
       {
-         // Access existing customer's items stored
+         // Access existing shopping cart's items
          var customerCart = await GetShoppingItems();
 
          // Create a new shopping cart if there are
@@ -75,18 +61,42 @@ namespace api.Controllers
          // Set reference to customer items and number of 
          // products in cart
          customerCart.AddCartItem(chosenProduct, quantity);
-
-         // Update database with added items to cart
          var storeProcedure = await _context.SaveChangesAsync();
 
-         // customer's request was successfull
          if (storeProcedure > 0)
          {
-            return StatusCode(201);
+            // Create a resource for shopping cart in the server,
+            // and add location's header in the response
+            return CreatedAtRoute("GetShoppingCart", MapCartItemsToDto(customerCart));
          }
 
-         // Notify customer there was a problem in current request 
-         return BadRequest(new ProblemDetails { Title = "Oops! There was a problem adding items to your cart. Try again" });
+         // Notify customer there was a problem in Post request 
+         return BadRequest(new ProblemDetails { Title = "Oops! Unable to add items to your cart. Try again" });
+      }
+
+      [HttpDelete]
+      public async Task<ActionResult> RemoveShoppingItemsInCart(int productId, int quantity)
+      {
+         // Access existing shopping cart's items
+         var customerCart = await GetShoppingItems();
+
+         if (customerCart == null)
+         {
+            return NotFound();
+         }
+
+         // Remove selected product based on number of items
+         // than is less than existing quantity
+         customerCart.RemoveCartItem(productId, quantity);
+         var storeProcedure = await _context.SaveChangesAsync();
+
+         if (storeProcedure > 0)
+         {
+            return Ok();
+         }
+
+         // Notify customer there was a problem with Delete request
+         return BadRequest(new ProblemDetails { Title = "Oops! Unable to remove items in your cart. Try again" });
       }
 
       private Cart CreateShoppingCart()
@@ -117,6 +127,25 @@ namespace api.Controllers
              // For every request and response a cookies is sent
              // from server to client in order to identify cart
              .FirstOrDefaultAsync(shoppingItems => shoppingItems.CustomerId == Request.Cookies["clientId"]);
+      }
+
+      private CartDto MapCartItemsToDto(Cart shoppingCart)
+      {
+         return new CartDto
+         {
+            Id = shoppingCart.Id,
+            CustomerId = shoppingCart.CustomerId,
+            CartItems = shoppingCart.CartItems.Select(cartItem => new CartItemDto
+            {
+               ProductId = cartItem.ProductId,
+               Name = cartItem.Product.Name,
+               Image = cartItem.Product.Image,
+               Brand = cartItem.Product.Brand,
+               Type = cartItem.Product.Type,
+               Price = cartItem.Product.Price,
+               Quantity = cartItem.Quantity
+            }).ToList()
+         };
       }
    }
 }
